9c934faeaded4eecdd54be41b7c2fd0ee96ed4e0
Dylan Blakemore <dylan.blakemore@gmail.com>
2024-04-19 14:04:00 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule QueryStats.MixProject do
   def project do
     [
       app: :query_stats,
-      version: "0.1.0",
+      version: "0.1.1",
       description: "Track Ecto queries in your test suite",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
